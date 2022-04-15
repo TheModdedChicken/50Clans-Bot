f36e9d79cfc737574010b07cfa059a0d4b7e1e2f
Logan Shaw <loganshaw1744@gmail.com>
2022-04-15 16:28:58 +0800

    man

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { AutocompleteInteraction, ButtonInteraction, CommandInteraction } from "discord.js";
+import { CommandInteraction } from "discord.js";
 
 export default {
   data: new SlashCommandBuilder().setName('ping').setDescription("Check the bot's ping"),
@@ -7,8 +7,5 @@ export default {
   async command(interaction: CommandInteraction) {
     const time = new Date().getTime() - interaction.createdAt.getTime();
     interaction.reply(`Pong! (${time}ms)`);
-  },
-
-  async autocomplete(interaction: AutocompleteInteraction) {},
-  async button(interaction: ButtonInteraction) {},
+  }
 }
